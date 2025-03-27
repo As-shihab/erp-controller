da58f44561538f1e79b818bc691ca1e356f53333
abdus salam shehab <ashehab@hybri.tech>
2025-03-28 00:55:43 +0600

    updated feature

---
+++ backend/app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Flat3\Lodata\Facades\Lodata;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        \Lodata::discover(\App\Models\User::class);
+
+    }
+}
